7a40f081ef7e6c963674fd102d4af6a9f5b7b4d1
项晨东 <2019011831@secoder.net>
2022-03-15 22:45:44 +0800

    #2 #1 fix style and gitlab-ci trying to get test pipline

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+"""
+注册模型用
+"""
+# from django.contrib import admin
 
 # Register your models here.
--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,13 @@
+"""
+app设置文件
+"""
+
 from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """
+    用于app的设置
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
