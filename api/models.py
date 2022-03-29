696c67848ce0bab4ea704f837facd09986877976
孙鸣泽 <2019010619@secoder.net>
2022-03-29 13:46:40 +0800

    #7 change field name in PrivateInfo alike ~Time to ~Date

--- api/models.py
+++ api/models.py
@@ -22,11 +22,10 @@ class PrivateInfo(models.Model):
     username = models.CharField(max_length=NAME_LEN)  # 用户用于登录的用户名
     avatar = models.ImageField()  # 用户头像
     bio = models.CharField(max_length=LONG_INFO_LEN)  # 签名
-    registrationDate = models.DateTimeField(null=True)  # 用户注册时间
     joinDate = models.DateTimeField(null=True)  # 用户入职时间
     employeeType = models.CharField(max_length=SHORT_INFO_LEN)  # 员工类型
-    graduationTime = models.DateTimeField(null=True)  # 毕业时间
-    registrationTime = models.DateTimeField(auto_now=True)  # 注册时间
+    graduationDate = models.DateTimeField(null=True)  # 毕业时间
+    registrationDate = models.DateTimeField(auto_now=True)  # 注册时间
     leader = models.CharField(max_length=NAME_LEN)  # 直属上级
     detail = models.CharField(max_length=1000)  # 入职情况和详细信息
     historicalMembers = models.IntegerField(default=0)  # 历史带新人数
