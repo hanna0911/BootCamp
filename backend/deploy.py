8ea5267b893cee6ab9511322d9ca47f6462e369f
孙鸣泽 <2019010619@secoder.net>
2022-04-02 09:05:44 +0000

    Update deploy.py

--- backend/deploy.py
+++ backend/deploy.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',  # TODO, uncomment this when csrf problem is fixed
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
