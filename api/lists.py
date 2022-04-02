3cf8bbdd015f0adc4d92a5e2daff049d77f76afc
项晨东 <2019011831@secoder.net>
2022-04-02 12:47:17 +0800

    #14 add nominate process api function ann testcases

--- api/lists.py
+++ api/lists.py
@@ -3,7 +3,7 @@ import logging
 
 from django.http import HttpRequest
 from .api_util import gen_response, role_authentication, load_private_info
-from .models import PrivateInfo
+from .models import PrivateInfo,UserProgramTable
 
 
 def admin_newcomer_list(request: HttpRequest):
@@ -40,6 +40,29 @@ def teacher_wait_list(req: HttpRequest):
 
 
 def nominate_process(req: HttpRequest):
+    username = req.session.get("username", None)
+    if username is None:
+        return gen_response(
+            400, message="no username in session, probly not login")
+    if not role_authentication(username, 'admin'):
+        return gen_response(400, message="no permission")
+    teacher_list = PrivateInfo.objects.filter(isTeacher=True, teacherIsDuty=False)
+    return_list = []
+    for teacher in teacher_list:
+        tmp = load_private_info(teacher)
+        tmp["teacherNominationDate"] = teacher.teacherNominationDate
+        status = teacher.teacherExaminedStatus
+        if status == 0:
+            tmp["teacherExaminedStatus"] = "未审核"
+        elif status == 1:
+            tmp["teacherExaminedStatus"] = "通过"
+        elif status == 2:
+            tmp["teacherExaminedStatus"] = "拒绝"
+        else:
+            raise Exception("数据可数据错误，请检查写入接口是否正确")
+        # program_relations = teacher.ProgramsAsUser.get()
+
+
     return gen_response(400, message="not supported")
 
 
