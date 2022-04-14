44a6bbb9e75029b2c1fecca1f005ce469cbf744d
项晨东 <2019011831@secoder.net>
2022-04-14 17:11:30 +0800

    #19 now can succefully return video stream

---
+++ api/download.py
@@ -0,0 +1,53 @@
+"""
+下载相关接口
+"""
+from django.http import HttpRequest, StreamingHttpResponse
+from wsgiref.util import FileWrapper
+import re
+import os
+import mimetypes
+
+
+def file_iterator(file_name, chunk_size=8192, offset=0, length=None):
+    with open(file_name, "rb") as f:
+        f.seek(offset, os.SEEK_SET)
+        remaining = length
+        while True:
+            bytes_length = chunk_size if remaining is None else min(remaining, chunk_size)
+            data = f.read(bytes_length)
+            if not data:
+                break
+            if remaining:
+                remaining -= len(data)
+            yield data
+
+
+def stream_video(request: HttpRequest):
+    """将视频文件以流媒体的方式响应"""
+    range_header = request.META.get('HTTP_RANGE', '').strip()
+    range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
+    range_match = range_re.match(range_header)
+    path = request.GET.get('path')
+    # 这里根据实际情况改变，我的views.py在core文件夹下但是folder_path却只到core的上一层，media也在core文件夹下
+    folder_path = os.getcwd().replace('\\', '/')
+    print(folder_path)
+    path = folder_path + '/static/' + path  # path就是template ？后面的参数的值
+    size = os.path.getsize(path)
+    content_type, encoding = mimetypes.guess_type(path)
+    content_type = content_type or 'application/octet-stream'
+    if range_match:
+        first_byte, last_byte = range_match.groups()
+        first_byte = int(first_byte) if first_byte else 0
+        last_byte = first_byte + 1024 * 1024 * 10
+        if last_byte >= size:
+            last_byte = size - 1
+        length = last_byte - first_byte + 1
+        resp = StreamingHttpResponse(file_iterator(path, offset=first_byte, length=length), status=206,
+                                     content_type=content_type)
+        resp['Content-Length'] = str(length)
+        resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
+    else:
+        resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+        resp['Content-Length'] = str(size)
+    resp['Accept-Ranges'] = 'bytes'
+    return resp
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views, lists, upload
+from . import views, lists, upload, download
 from backend.write_db import write_db
 
 # api应用的路由配置
@@ -7,7 +7,7 @@ urlpatterns = [
     path("write_db", write_db),
     path('login', views.login, name='login'),  # just for test
     path('join', views.join, name='join'),  # just for join
-    path("get_user_info",views.get_user_info,name="get_user_info"),
+    path("get_user_info", views.get_user_info, name="get_user_info"),
     path('switch_role', views.switch_role, name='switch_role'),  # 切换身份
     path('logout', views.logout, name='logout'),  # 登出
     path("admin_newcomer_list", lists.admin_newcomer_list, name="admin_newcomer_list"),
@@ -20,4 +20,5 @@ urlpatterns = [
     path("create_program", upload.create_program, name="create_program"),
     path("admin_create_content_template", upload.create_content, name="create_content_template"),
     path("admin_create_lesson_template", upload.create_lesson, name="create_lesson_template"),
+    path("video/", download.stream_video, name="video")
 ]
