e15aee5b566cfc555b8d6c3913f9514679727b27
孙鸣泽 <2019010619@secoder.net>
2022-04-29 21:42:39 +0800

    #38 debug test for unit-test yml

--- backend/deploy.py
+++ backend/deploy.py
@@ -114,7 +114,7 @@ LANGUAGE_CODE = 'en-us'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = False  # ！！！！！，不要设为True，否则我们会花更多的精力fix之前的东西  by smz
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
