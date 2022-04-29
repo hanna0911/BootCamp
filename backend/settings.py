3f2536c39b048a9ccd260ef2da9549c1434fad21
孙鸣泽 <2019010619@secoder.net>
2022-04-29 20:09:59 +0800

    #38 debug test for unit-test yml

--- backend/settings.py
+++ backend/settings.py
@@ -119,7 +119,7 @@ LANGUAGE_CODE = 'en-us'
 USE_TZ = False
 USE_I18N = True
 
-# USE_TZ = True
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
