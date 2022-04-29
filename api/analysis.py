fc6076c53274060de15812cd4b0ae5dd4b72cd5f
孙鸣泽 <2019010619@secoder.net>
2022-04-29 17:50:31 +0800

    #38 add analysis_precheck function

--- api/analysis.py
+++ api/analysis.py
@@ -99,33 +99,11 @@ def bootcamp_attend(request: HttpRequest):
     bootcamp参与率
     传入查询起始日期和结束日期
     """
-    if request.method != "POST":
-        return illegal_request_type_error_response()
+    result = analysis_precheck(request)
 
-    try:
-        data = json.loads(request.body)
-    except JSONDecodeError:
-        return gen_response(400, "JSON format error")
-
-    try:
-        session = request.session
-        role = session["role"]
-    except KeyError:
-        return session_timeout_response()
-
-    if role not in ["admin", "HRBP"]:
-        return unauthorized_action_response()
-
-    try:
-        startDate = data["dateRangeStart"]
-        startDate = datetime.datetime.fromtimestamp(startDate / 1000)
-        endDate = data["dateRangeEnd"]
-        endDate = datetime.datetime.fromtimestamp(endDate / 1000)
-    except KeyError:
-        return gen_response(400, "JSON format error")
-
-    if not (check_day(startDate, True) and check_day(endDate, False)):
-        return gen_response(400, "Invalid date range")
+    if isinstance(result, HttpResponse):
+        return result
+    startDate, endDate, dept = result
 
     days = []
     totalEmploy = []
@@ -159,39 +137,11 @@ def newcomer_average_score(request: HttpRequest):
     """
     新人平均分
     """
-    if request.method != "POST":
-        return illegal_request_type_error_response()
+    result = analysis_precheck(request)
 
-    try:
-        data = json.loads(request.body)
-    except JSONDecodeError:
-        return gen_response(400, "JSON format error")
-
-    try:
-        session = request.session
-        role = session["role"]
-        username = session["username"]
-    except KeyError:
-        return session_timeout_response()
-
-    if role not in ["admin", "HRBP"]:
-        return unauthorized_action_response()
-
-    try:
-        dept = PrivateInfo.objects.get(username = username).dept
-    except Exception:
-        return session_timeout_response()
-
-    try:
-        startDate = data["dateRangeStart"]
-        startDate = datetime.datetime.fromtimestamp(startDate / 1000)
-        endDate = data["dateRangeEnd"]
-        endDate = datetime.datetime.fromtimestamp(endDate / 1000)
-    except KeyError:
-        return gen_response(400, "JSON format error")
-
-    if not (check_day(startDate, True) and check_day(endDate, False)):
-        return gen_response(400, "Invalid date range")
+    if isinstance(result, HttpResponse):
+        return result
+    startDate, endDate, dept = result
 
     users = PrivateInfo.objects.filter(newcomerGraduateDate__range = (startDate, endDate))
 
